aa974eda2882c0f1458e3c3f3158a22ba5a99523
Shatodru <shatodru2003@gmail.com>
2023-10-30 23:15:55 +0530

    Entry 18

---
+++ Asynchronous JS/AsyncAwait.js
@@ -0,0 +1,30 @@
+// If we put async infront of a function, it returns a promise automatically.
+
+// Structure examples:
+
+
+// async function hello(){}
+
+// OR
+
+// const sing = async() => {}
+
+
+const login = async(username, password) => {
+    if (!username || !password)
+    throw "Missing Credentials"
+
+    if (password === "ricknmorty")
+    return "Welcome"
+    throw "Invalid Password"
+}
+
+login("agydygdwagiydaw", "ricknmorty")
+.then(msg => {
+    console.log("Logged in!")
+    console.log(msg)
+})
+.catch(err => {
+    console.log("Error!!!")
+    console.log(err)
+})
--- Asynchronous JS/callbackPractice.js
+++ Asynchronous JS/callbackPractice.js
@@ -16,20 +16,64 @@
 
 
 
-const delayedColorChange = (newColor, delay, doNext) => {
-    setTimeout(() => {
-document.body.style.backgroundColor = newColor
-doNext()
-    }, delay)
-}
+// const delayedColorChange = (newColor, delay, doNext) => {
+//     setTimeout(() => {
+// document.body.style.backgroundColor = newColor
+// doNext()
+//     }, delay)
+// }
 
-delayedColorChange("red", 2000, () => {
-    delayedColorChange("teal", 2000, () => {
-    delayedColorChange("yellow", 2000, () => {
-    delayedColorChange("black", 2000, () => {
-})
-})
-})
-})
+// delayedColorChange("red", 2000, () => {
+//     delayedColorChange("teal", 2000, () => {
+//     delayedColorChange("yellow", 2000, () => {
+//     delayedColorChange("black", 2000, () => {
+// })
+// })
+// })
+// })
 
 // Remember that all that matters is setting the variables and knowing where they would end up mapping themselves. The logic stays the same.
+
+
+
+// An even more efficient way:
+
+const delayedColorChange = (color, delay) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            document.body.style.backgroundColor = color
+            resolve()
+        }, delay)
+    })
+}
+
+// delayedColorChange("red", 1000)
+// .then(() => delayedColorChange("orange", 1000))
+// .then(() => delayedColorChange("teal", 1000))
+// .then(() => delayedColorChange("green", 1000))
+// .then(() => delayedColorChange("blue", 1000))
+// .then(() => delayedColorChange("violet", 1000))
+
+
+// Now using async and await:
+
+async function rainbow() {
+    await delayedColorChange("red", 1000)
+    await delayedColorChange("orange", 1000)
+    await delayedColorChange("yellow", 1000)
+    await delayedColorChange("green", 1000)
+    await delayedColorChange("blue", 1000)
+    await delayedColorChange("indigo", 1000)
+    await delayedColorChange("violet", 1000)
+    return "All done!"
+}
+
+// We can apply then after an entire function is executed completely as well. (Uncomment the code below to see)
+// rainbow().then(() => console.log("End of rainbow!"))
+
+// OR
+async function waitForRainbow() {
+    await rainbow()
+    console.log("End of rainbow!")
+}
+// Basically the same code as before.
