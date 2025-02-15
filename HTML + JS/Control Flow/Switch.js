c5c0f512f6450c93d9f03887b1b0ac1449a2875d
Shatodru <shatodru2003@gmail.com>
2025-02-15 15:06:09 +0530

    15/02/2025

---
+++ HTML + JS/Control Flow/Switch.js
@@ -0,0 +1,23 @@
+const month = 2 // Enter any value and recieve the month that corresponds with the value.
+
+switch (month) {
+    case 1:
+        console.log("january")
+        break;
+    case 2:
+        console.log("february")
+        break;
+    case 3:
+        console.log("march")
+        break;
+    case 4:
+        console.log("april")
+        break;
+    case 5:
+        console.log("may")
+        break;
+
+    default:
+        console.log("Default case match")        
+        break;
+}
